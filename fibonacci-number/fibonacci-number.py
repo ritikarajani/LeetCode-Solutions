0acb6f3c88a6ee2e7fb370f0893872cc034961b3
ritikarajani <50978594+ritikarajani@users.noreply.github.com>
2021-08-19 21:52:36 +0530

    Time: 24 ms (95.17%), Space: 14.1 MB (89.78%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        a, b, c = 0, 1, 1
+        for _ in range(1,n):
+            c = a + b                
+            a, b = b, c
+        return c
