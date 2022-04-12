511eb1052ca9cd7c66e078b3c02bc8a22b8cb2cf
ritikarajani <50978594+ritikarajani@users.noreply.github.com>
2022-04-12 13:37:04 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.py
@@ -0,0 +1,47 @@
+ #User function Template for python3
+ 
+class Solution:
+    
+    # arr[] : the input array
+    # N : size of the array arr[]
+    
+    #Function to return length of longest subsequence of consecutive integers.
+    def findLongestConseqSubseq(self,arr, N):
+        n = set(arr)
+        length = 0
+        for i in arr:
+            if i-1 not in n:
+                currentNum = i
+                currentLen = 1
+                while (currentNum+1) in n:  #find all nums of a consecutive sequence
+                    currentLen+=1
+                    currentNum+=1
+                length = max(length, currentLen)
+        return length
+                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n=int(input())
+        a = list(map(int, input().strip().split()))
+        print(Solution().findLongestConseqSubseq(a,n))
+# } Driver Code Ends
