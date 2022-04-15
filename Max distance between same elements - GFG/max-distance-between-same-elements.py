1f5787803af61af82d7595b71f5033658738b62e
ritikarajani <50978594+ritikarajani@users.noreply.github.com>
2022-04-15 19:19:48 +0530

    Added solution - LeetHub

---
+++ Max distance between same elements - GFG/max-distance-between-same-elements.py
@@ -0,0 +1,27 @@
+class Solution:
+    # Your task is to Complete this function
+    # functtion should return an integer
+    def maxDistance(self, arr, n):
+       # Code here
+       mp={}
+       maxDist=0
+       for i in range(n):
+           if arr[i] not in mp.keys():
+               mp[arr[i]]=i
+           else:
+               maxDist=max(maxDist,i-mp[arr[i]])
+       return maxDist 
+
+#{ 
+#  Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob=Solution()
+        print(ob.maxDistance(arr, n))
+# Contrbuted By:Harshit Sidhwa
+
+
+# } Driver Code Ends
